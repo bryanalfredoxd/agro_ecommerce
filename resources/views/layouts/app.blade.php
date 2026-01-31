dd02ffb44a710374a9901bb38e9c72005f211031
DeltaSasaki <chanocorro1@gmail.com>
2026-01-31 14:59:54 -0400

    Icono almacenado 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,7 @@
     
     <link href="https://fonts.googleapis.com" rel="preconnect">
     <link crossorigin href="https://fonts.gstatic.com" rel="preconnect">
-    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
-    
-    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet">
+
     
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     
