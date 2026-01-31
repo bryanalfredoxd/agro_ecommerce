8f84e162ee2b18bb7a3cade2ede8814a380853b0
bryanalfredoxd <bryanalfredoxd@gmail.com>
2026-01-31 14:23:38 -0400

    prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+<div class="fixed top-0 left-0 w-full z-[100] bg-yellow-400 text-black text-center font-black py-2 shadow-[0_020px#13ec13] border-b-4 border-primary animate-bounce">
+    SI VES ESTA BARRA AMARILLA REBOTANDO, TAILWIND ESTÁ FUNCIONANDO
+</div>
+
 @section('title', 'Agropecuaria Venezuela - Soluciones Integrales para el Campo')
 
 @section('content')
